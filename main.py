efd5e77d3f38f0b9993d412a2a3ca0d9e35a29cc
jpala71 <jpala71@wgu.edu>
2025-01-29 20:47:03 -0500

    Creates the Package Object

--- main.py
+++ main.py
@@ -52,3 +52,32 @@ class ChainingHashTable:
         for key_value in bucket_list:
             if key_value[0] == key:
                 bucket_list.remove([key_value[0],key_value[1]])
+
+#Creates the package object
+class Package:
+    def __init__(self, package_id, address, city, state, zip_code, deadline, weight, notes=""):
+        self.package_id = package_id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.deadline = deadline
+        self.weight = weight
+        self.notes = notes
+        self.status = "At the Hub"
+        self.departure_time = None
+        self.delivery_time = None
+
+    #Updates the status of the package
+    def update_status(self, currentTime):
+        if self.delivery_time and currentTime >= self.delivery_time:
+            self.status = "Delivered"
+        elif self.departure_time and currentTime >= self.departure_time:
+            self.status = "En Route"
+        else:
+            self.status = "At the Hub"
+
+    def __str__(self):
+        return (f"Package ID: {self.package_id}, Address: {self.address}, {self.city}, {self.state}, {self.zip_code}, "
+                f"Deadline: {self.deadline}, Weight: {self.weight} KILO, Status: {self.status}, "
+                f"Departure Time: {self.departure_time}, Delivery Time: {self.delivery_time}")
